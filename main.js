67599cf026cb299ed746e637427c0aa4f6b0e272
Betsy Gillard <104951899+beegillard@users.noreply.github.com>
2023-03-29 16:48:12 -0600

    add js and change some formatting

--- main.js
+++ main.js
@@ -1,8 +1,43 @@
-document.body.innerHTML = '
+const first = [
+    'Today is your day',
+'Today is not your day',
+'The time is now',
+'Tomorrow will be your day'
+];
 
-<nav class="top-nav">
-    <div id="about"><h2>About Me</h2></div>
-    <div id="resume"><h2>Resume</h2></div>
-    <div id="projects"><h2>Projects</h2></div>
-</nav>
-'
+const second = [
+    'to go cause some trouble',
+'to take some time for introspection',
+'to give BirdDog some treats',
+'to eat a lot of cookies',
+];
+
+const third = [
+'the difference between "they\'re" "there" and "their".',
+'capitalism is a death cult.',
+'to eat breakfast first.',
+'I just took a DNA test turns out I\'m 100% that bitch.',
+];
+
+
+
+const generateSentence = () => {
+    let one = first[Math.floor(Math.random() * 4)];
+    let two = second[Math.floor(Math.random() * 4)];
+    let three = third[Math.floor(Math.random() * 4)];
+
+    return `${one} ${two}, but don't forget ${three}`;
+}
+
+const giveAdvice = () => {
+    let sentence = generateSentence();
+    document.getElementById('empty').innerHTML = sentence;
+    document.querySelector('button').innerHTML = 'More advice please';
+
+}
+
+
+
+
+let button = document.querySelector('button');
+button.addEventListener('click', giveAdvice);
