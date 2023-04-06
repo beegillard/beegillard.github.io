d6b6fc5547aee60705ff4b5ea3ac4ece4938aa10
Betsy Gillard <104951899+beegillard@users.noreply.github.com>
2023-04-06 14:43:46 -0600

    finish desktop pages

--- main.js
+++ main.js
@@ -2,20 +2,23 @@ const first = [
     'Today is your day to',
 'Today it is definitely a bad idea to',
 'The time is now to',
-'When in doubt,'
+'When in doubt,',
+'The magic 8 ball says:',
 ];
 
 const second = [
-    ' go cause some trouble',
-' take some time for introspection',
-' give BirdDog some treats',
-' eat a lot of cookies',
+    'go cause some trouble',
+'take some time for introspection',
+'give BirdDog some treats',
+'eat a lot of cookies',
+'drink a glass of water'
 ];
 
 const third = [
-    ", but don't forget",
+    ",but don't forget",
     ", oh and also",
-    ", but you're a terrible person if you didn't know"
+    ", but you're a terrible person if you didn't know",
+    ", and have you considered the fact that",
 ];
 
 const fourth = [
@@ -33,7 +36,7 @@ const generateSentence = () => {
     let three = third[Math.floor(Math.random() * third.length)];
     let four = fourth[Math.floor(Math.random() * fourth.length)];
 
-    return `${one}${two}${three}${four}`;
+    return `${one} ${two} ${three} ${four}`;
 }
 
 let button = document.getElementById('advice-button');
